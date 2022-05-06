eafe6654213b1d0274dbbadacc293ee00cef5482
Alisher Kenzhebayev <alisher.kenzhebayev@alumni.nu.edu.kz>
2022-05-07 01:03:50 +0200

    Working Gerstner + fix axis
    remapped wrong axis y -> z

--- WaterShader/SineTextureShaderClass.cpp
+++ WaterShader/SineTextureShaderClass.cpp
@@ -361,13 +361,13 @@ bool SineTextureShaderClass::SetShaderParameters(ID3D11DeviceContext* deviceCont
 	dataSPtr = (SineBufferType*)mappedResourceS.pData;
 
 	dataSPtr->commonConst =		DirectX::XMFLOAT4(0.0, 0.5, 1.0, 2.0);
-	dataSPtr->waveHeights =		DirectX::XMFLOAT4(2.0, 3.5, 0.25, 0.1);
-	dataSPtr->waveLengths =		DirectX::XMFLOAT4(0.1, 0.3, 0.5, 0.4);
+	dataSPtr->waveHeights =		DirectX::XMFLOAT4(5.0, 3.5, 1.5, 1);
+	dataSPtr->waveLengths =		DirectX::XMFLOAT4(10, 3, 5, 4);
 	dataSPtr->waveOffset =		DirectX::XMFLOAT4(-0.5f, 0.2f, 0.45f, 0.0f);
-	dataSPtr->waveSpeed =		DirectX::XMFLOAT4(0.2, 0.15, 0.4, 0.3);
+	dataSPtr->waveSpeed =		DirectX::XMFLOAT4(2.5, 0.75, 1, 1.5);
 	dataSPtr->waveDirx =		DirectX::XMFLOAT4(0.25, 0.0, -0.7, -0.8);
 	dataSPtr->waveDiry =		DirectX::XMFLOAT4(0.0, 0.45, -0.7, 0.1);
-	dataSPtr->Q =				DirectX::XMFLOAT4(0, 0.2, 0.5, 1);
+	dataSPtr->Q =				DirectX::XMFLOAT4(0.1, 0.2, 0.5, 1);
 	dataSPtr->K =				DirectX::XMFLOAT4(2, 3, 2, 3);
 	dataSPtr->bumpSpeed =		DirectX::XMFLOAT4(0.031, 0.04, -0.03, 0.02);
 	dataSPtr->piVector =		DirectX::XMFLOAT4(4.0, 1.57079632, 3.14159265, 6.28318530);
--- WaterShader/data/shaders/SimpleSinVS.hlsl
+++ WaterShader/data/shaders/SimpleSinVS.hlsl
@@ -11,9 +11,9 @@ cbuffer SineBuffer : register(b1)
     float4 commonConst;
     float4 waveHeights;
     float4 waveLengths;
-    float4 waveOffset; 
-    float4 waveSpeed; 
-    float4 waveDirx; 
+    float4 waveOffset;
+    float4 waveSpeed;
+    float4 waveDirx;
     float4 waveDiry;
     float4 Q;
     float4 K;
@@ -21,10 +21,10 @@ cbuffer SineBuffer : register(b1)
     float4 piVector;
     float4 sin7;
     float4 cos8;
-    float4 frcFixup; 
-    float4 psCommonConst; 
+    float4 frcFixup;
+    float4 psCommonConst;
     float4 highlightColor;
-    float4 waterColor; 
+    float4 waterColor;
     float4 time;
 };
 
@@ -39,12 +39,12 @@ struct PixelInputType
 {
     float4 position : SV_POSITION;
     float3 normal : NORMAL;
-    float2 texUV: TEXCOORD0;
+    float2 texUV : TEXCOORD0;
 };
 
-float CalcQ(float Q, float freq, float ampl, float numWaves = 2)
+float4 CalcQ(float4 Q, float4 freq, float4 ampl, float4 numWaves = 2)
 {
-    return Q / (freq * ampl * numWaves);
+    return Q / (freq * ampl);
 }
 
 PixelInputType SimpleSinVertexShader(VertexInputType input)
@@ -54,14 +54,17 @@ PixelInputType SimpleSinVertexShader(VertexInputType input)
     // w
     float4 frequency = sqrt((9.81 * piVector.w) / waveLengths);
     
-    // dot(Di * (x, y)) - Taking as point of reference the UV mapping
-    float4 dotX = mul(waveDirx, input.texUV.x);
-    float4 dotY = mul(waveDiry, input.texUV.y);
-    float4 bracketValue =   dotX + dotY;
+    float4 x = input.texUV.x;
+    float4 y = input.texUV.y;
     
-    // This is ill-advised, as the values are a multiple times harder to calculate and set up + requires GPU side tesselation to look normal.
-    //float4 bracketValue = mul(waveDirx, input.position.x * scale); 
-    //bracketValue += mul(waveDiry, input.position.y * scale);
+    // dot(Di * (x, y)) - Taking as point of reference the UV mapping
+    float4 dotX = mul(waveDirx, x);
+    float4 dotY = mul(waveDiry, y);
+    float4 bracketValue = dotX + dotY;
+    
+    /*// This is ill-advised, as the values are a multiple times harder to calculate and set up + requires GPU side tesselation to look normal.
+    float4 bracketValue = mul(waveDirx, input.position.x * scale); 
+    bracketValue += mul(waveDiry, input.position.y * scale);*/
     
     // *= w
     bracketValue = mul(bracketValue, frequency);
@@ -73,16 +76,24 @@ PixelInputType SimpleSinVertexShader(VertexInputType input)
     float4 sinValue = sin(bracketValue);
     float4 cosValue = cos(bracketValue);
     
-    // Starting off the Eq 8.1
+    // Starting off the Eq 8.1, mode in additive sines
     sinValue += 1.0;
     sinValue /= 2.0;
     sinValue = pow(sinValue, K);
     
-    //TODO: vertex displacement
-    //if(input.position.y % 2 == 0)
-    input.position.xyz += input.normal * dot(sinValue, waveHeights) * 2;
+    // Calculating Gerstner waves
+    float4 valSteepness = mul(waveHeights, CalcQ(Q, frequency, waveHeights));
+    float4 calcX = mul(dotX, cosValue);
+    calcX = dot(calcX, valSteepness);
+    calcX *= cosValue;
+    float4 calcY = mul(dotY, cosValue);
+    calcY = dot(calcY, valSteepness);
+    calcY *= cosValue;
     
-    input.position.x += 1;
+    // Vertex displacement
+    input.position.x += calcX;
+    input.position.z += calcY;
+    input.position.y = dot(sinValue, waveHeights);
     
     // Calculate the position of the vertex against the world, view, and projection matrices.
     output.position = mul(input.position, worldMatrix);
@@ -90,7 +101,7 @@ PixelInputType SimpleSinVertexShader(VertexInputType input)
     output.position = mul(output.position, projectionMatrix);
     
     //TODO: update normals based on sine eq
-    output.normal = sinValue.www; //sin(bracketValue.x).xxx;
+    output.normal = sinValue.xxx; //sin(bracketValue.x).xxx;
     // Normalize the normal vector.
     //output.normal = normalize(output.normal);
     
